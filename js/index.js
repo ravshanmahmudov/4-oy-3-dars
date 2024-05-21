f6a3d3b6395373d6bc868b6c58e427a3c1be3016
ravshanmahmudov <ravshanmahmudov882@gmail.com>
2024-05-22 00:54:52 +1400

    misolar xamasi

---
+++ js/index.js
@@ -0,0 +1,213 @@
+// 1chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// if (a>0){
+// console.log(`${a} soni musbat`);
+// }
+// else{
+//   console.log(`${a} soni musbat emas`);
+// }
+
+// 2chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// if (a % 2==1){
+// console.log(`${a} toq son`);
+// }
+// else{
+//   console.log(`${a} toq son emas`);
+// }
+
+// 3chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// if (a%2==0){
+// console.log(`${a} juft son`);
+// }
+// else{
+//   console.log(`${a} juft son emas`);
+// }
+
+// 4chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("a");
+// if (a >2 || b<=3) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 5chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("a");
+// if (a >= 0 || b < -2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 6chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("a");
+// let c = +prompt("c");
+// if (a <=b  && b <=c) {
+//   console.log(true);
+// } else if (a<=c){
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+// 8chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("a");
+// if (a % 2==1 && b%2==1){
+//   console.log( true );
+// }
+// else{
+//   console.log(false );
+// }
+
+// 9chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("a");
+// if (a % 2==1 || b%2==1){
+//   console.log( true );
+// }
+// else{
+//   console.log(false );
+// }
+
+// 10chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("a");
+// if (a % 2==1 || b % 2==1){
+//   console.log( true );
+// }
+// else{
+//   console.log(false );
+// }
+
+// 11chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("a");
+// if (a % 2==1 && b % 2==1 || a%2==0 && b%2==0){
+//   console.log( true );
+// }
+// else{
+//   console.log(false );
+// }
+
+// 12chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("b");
+// let c = +prompt("c");
+// if (a >= 0 && b >= 0) {
+//   console.log(false);
+// }
+// else if (a >= 0 && c >= 0) {
+//   console.log(false);
+// }
+// else if (b >= 0 && c >= 0) {
+//   console.log(false);
+// }
+// else {
+//   console.log(true);
+// }
+
+// 13chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("b");
+// let c = +prompt("c");
+// if (a >= 0 && b >= 0) {
+//   console.log(true);
+// }
+// else if (a >= 0 || c >= 0) {
+//   console.log(true);
+// }
+// else if (b >= 0 || c >= 0) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+// 14chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("b");
+// let c = +prompt("c");
+// if (a >= 0 || b >= 0) {
+//   console.log(true);
+// }
+// else if (a >= 0 || c >= 0) {
+//   console.log(true);
+// }
+// else if (b >= 0 || c >= 0) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+// 15chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// let b = +prompt("b");
+// let c = +prompt("c");
+// if (a >= 0 && b >= 0) {
+//   console.log(true);
+// }
+// else if (a >= 0 && c >= 0) {
+//   console.log(true);
+// }
+// else if (b >= 0 && c >= 0) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+// 16chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// if (a > 9 && a < 100 && a % 2 == 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 17chi masala
+
+// alert("son");
+// let a = +prompt("a");
+// if (a > 100 && a < 1000 &&  a % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
